badfd644eea5927b6037faddbb74a9fa3799a93d
jwowo <jongwoo0221@gmail.com>
2021-11-07 01:25:27 +0900

    Change typo in jongwoo_9663.py

--- baekjoon/9663/jongwoo_9663.py
+++ baekjoon/9663/jongwoo_9663.py
@@ -1,8 +1,12 @@
-n = int(input())
+# 문제 접근 방법
+# 백트래킹 & DFS 이용
+# ( 조건 1 )같은 열(세로)에는 하나의 퀸만 놓을 수 있다.
+# 길이가 5인 배열 c 생성 ( 인덱스 : 열 인덱스, 값 : 행 인덱스 )
+# ( e.g ) c[1] = 4 이면, ( 4 , 1 )에 퀸이 있음
+# DFS를 돌면서 가로, 대각선에 퀸을 놓을 수 있는지 확인
+# DFS를 돌면서 n 번째 퀸을 놓았을때 count += 1
 
-c = [ [0] * n for _ in range(n + 1) ]
+import sys
 
-for i in range(1, n+1):
-    for j in range(1, n+1):
-
+n = int(sys.stdin.readline())
 
