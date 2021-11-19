3f18ec19446bc01a5614ffb7f6f791e068825168
jwowo <jongwoo0221@gmail.com>
2021-11-20 00:18:32 +0900

    백준 2606 바이러스 문제 해결함.

---
+++ baekjoon/2606/2606_바이러스.py
@@ -0,0 +1,40 @@
+"""백준 2606 바이러스"""
+
+"""
+문제 접근 방법
+bfs해서 방문하지 않은 컴퓨터에 접근할때마다 count += 1 한다.
+"""
+
+import sys
+from collections import deque
+
+computers = int(sys.stdin.readline())
+networks = int(sys.stdin.readline())
+connections = [ [0] * (computers + 1) for _ in range(computers + 1) ]
+visited = [False] * (computers + 1)
+count = 0
+
+
+def bfs(x):
+    global count
+    # 큐에 삽입하고 방문처리한다.
+    queue = deque()
+    queue.append(x)
+    visited[x] = True
+
+    while queue:
+        # 큐에서 빼서 인접한 노드를 큐에 넣고 방문처리
+        v = queue.popleft()
+        for i in range(1, computers + 1):
+            if not visited[i] and connections[v][i] == 1:
+                count += 1
+                queue.append(i)
+                visited[i] = True
+
+
+for _ in range(networks):
+    a, b = map(int, sys.stdin.readline().split())
+    connections[a][b] = connections[b][a] = 1
+
+bfs(1)
+print(count)
